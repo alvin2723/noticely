a409250a07121abf0606b574cca70865f301e97a
alvin2723 <55695349+alvin2723@users.noreply.github.com>
2020-10-27 18:57:48 +0700

    update detail sm add mom

--- resources/views/livewire/post/detailmom.blade.php
+++ resources/views/livewire/post/detailmom.blade.php
@@ -1,3 +1,81 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <div class="card rounded shadow" style="margin-right:22%">
+        <div class="card-body p-5">
+           <h3 class="pb-4">Minute of Meeting Details</h3>
+         
+           <table class="table table-hover border">
+            <tbody>
+              <tr class="text-center">
+                <td class="border-right blue-color"><h4>Mark</h4></td>
+                <td class=>Otto</td>
+                <td></td>
+                <td></td>
+                
+              </tr>
+              <tr class="text-center">
+                <td class="border-right blue-color"><h4>Mark</h4></td>
+                <td>Thornton</td>
+                <td></td>
+                <td></td>
+               
+              </tr>
+              <tr class="text-center" style="">
+                <td class="border-right blue-color"><h4>Mark</h4></td>
+                <td class="border-right">Thornton</td>
+                <td class="border-right blue-color"><h4>Mark</h4></td>
+                <td>sdfsdf</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="card">
+            <div class="card-header blue-color">
+                <h4>sdfsfdsdf</h4>
+            </div>
+            <div class="card-body">
+              <p>sdfsdfsdf</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header blue-color">
+                <h4>sdfsfdsdf</h4>
+            </div>
+            <div class="card-body">
+                
+                 <p>sdfsdfsdf</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header blue-color">
+                <h4>sdfsfdsdf</h4>
+            </div>
+            <div class="card-body" style="padding: 0;">
+                <table class="table table-hover border" style="margin:0;">
+                    <tbody>
+                      <tr class="text-center">
+                        <td class="border-right"><h4>Mark</h4></td>
+                        <td class=>Otto</td>
+                        <td></td>
+                        <td></td>
+                        
+                      </tr>
+                      <tr class="text-center">
+                        <td class="border-right"><h4>Mark</h4></td>
+                        <td>Thornton</td>
+                        <td></td>
+                        <td></td>
+                       
+                      </tr>
+                      <tr class="text-center" style="">
+                        <td class="border-right"><h4>Mark</h4></td>
+                        <td class="border-right">Thornton</td>
+                        <td class="border-right"><h4>Mark</h4></td>
+                        <td>sdfsdf</td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
+          </div>
+          
+        </div>
+    </div>
 </div>
--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,7 +1,7 @@
 <div class="row"  style="margin: 0">
 
     {{-- Nothing in the world is as soft and yielding as water. --}}
-    <div class="col-7">
+    <div class="col-7 col-lg-8 col-md-7">
         <div class="card rounded shadow">
             <div class="card-body p-5">
                 <div class="row">
@@ -17,7 +17,7 @@
                                 <p class="mx-4">sdffd</p>
                             </div>
                             <div>
-                                <a href="#" type="button" class="btn btn-primary text-right px-4">Details <i class="fas fa-angle-double-down" style="margin-left:10px"></i>
+                                <a href="{{route('post.detail-mom')}}" type="button" class="btn btn-primary text-right px-4">Details <i class="fas fa-angle-double-down" style="margin-left:10px"></i>
                                 </a>
                             </div>
 
@@ -65,7 +65,7 @@
     </div>
     
   
-    <div class="col-5 right-box">
+    <div class="col-5 col-lg-4 col-md-5 right-box">
         
         <a href="{{route('post.create-mom')}}" type="button" class="btn btn-primary btn-lg px-4"><i class="fas fa-plus" style="margin-right: 10px"></i>
             Create MoM</a>
